be930f216ebc278c6be2287d1914ea8fafb2a628
shalin <shalin.rono@student.moringaschool.com>
2021-10-26 11:19:03 +0300

    add services routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SubjectsComponent } from './subjects/subjects.component';
+import { AddUpdateComponent } from './subjects/add-update/add-update.component';
+import { ShowComponent } from './subjects/show/show.component';
+import { ServicesService } from './services.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubjectsComponent
+    SubjectsComponent,
+    AddUpdateComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
