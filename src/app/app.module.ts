97577eb04a56dddc88a62753b5c8ed3f5b37bd2a
shalin <shalin.rono@student.moringaschool.com>
2021-10-26 11:11:39 +0300

    create angular app and generate subjects components

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SubjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
