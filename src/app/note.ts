cf5c65fea902266469e58b967c5244bddf6988e4
shalin <shalin.rono@student.moringaschool.com>
2021-10-28 21:16:25 +0300

    create class notes

--- src/app/note.ts
+++ src/app/note.ts
@@ -1,2 +1,11 @@
 export class Note {
-}
+    constructor(
+      public id: number,
+      public user: number,
+      public title: string,
+      public description: string,
+      public subject: string,
+      public created_at: Date,
+      public updated_at: Date
+    ) {}
+  }
