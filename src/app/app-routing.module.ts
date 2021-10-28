7f3bd948a04b275dde00ba62dac8aac3f63d8084
shalin <shalin.rono@student.moringaschool.com>
2021-10-28 12:04:58 +0300

    add register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {SubjectsComponent} from './subjects/subjects.component';
 import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
   {path:'subjects',component:SubjectsComponent},
-  {path:'home',component:HomeComponent}
+  {path:'home',component:HomeComponent},
+  {path:'register',component:RegisterComponent}
 
 ];
 
